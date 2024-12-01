85ff84bab774162bedcd588cd29de4fccde4d14c
朱科羽 <379389113@qq.com>
2024-12-01 22:50:38 +0800

    数组类型

--- index.dart
+++ index.dart
@@ -1,5 +1,9 @@
 void main(List<String> args) {
-    var a = 1;
-    int b = 2;
-    print(a+b);
+    var l1 =["障碍",20,true];
+    print(l1);
+    print(l1[0]);
+    print(l1[1]);
+    print(l1[2]);
+    print(l1.length);
+  var l2 = <String>["障碍", "20", "true"];  // All elements are now strings
 }
