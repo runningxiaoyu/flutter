f774f82f363174ca02c4c7eadd0a44ad6041dfaa
朱科羽 <379389113@qq.com>
2024-12-02 22:25:42 +0800

    main 函数的参数列表

--- index.dart
+++ index.dart
@@ -1,8 +1,15 @@
 void main(List<String> args) {
-  var str = 3232;
-  if (str is String) {
-    print(str);
-  }else if (str is int) {
-    print(str);
+  // 打印所有传入的命令行参数
+  print('命令行参数列表：$args');
+  
+  // 如果需要使用整数，可以将字符串转换为整数
+  for (var arg in args) {
+    // 尝试将字符串转换为整数
+    try {
+      int number = int.parse(arg);
+      print('参数转换为数字: $number');
+    } catch (e) {
+      print('参数 $arg 不能转换为数字');
+    }
   }
 }
