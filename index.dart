7ddfff039ce01511b786cb9516cf940c8dd8bcf1
朱科羽 <379389113@qq.com>
2024-12-03 11:00:42 +0800

    try catch

--- index.dart
+++ index.dart
@@ -1,13 +1,9 @@
 void main(List<String> args) {
-   var person = {
-      "name": "张三",
-      "age": 18
-   };
-   print(person["name"]);
-
-   var p = new Map();
-   p["name"] = "张三";
-   p["age"] = 18;
-   p["hobby"]=['篮球','足球'];
-   print(p["hobby"][1]);
+  String str = '';
+  try {
+    var myNum = int.parse(str);
+    print(myNum is int);
+  } catch (e) {
+    print('could not parse: $e');
+  }
 }
