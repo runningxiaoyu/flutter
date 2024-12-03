c06b629f6e4c0a17a163d72a4e8d4c6624f0f2d0
朱科羽 <379389113@qq.com>
2024-12-03 09:59:30 +0800

    三目元算符

--- index.dart
+++ index.dart
@@ -1,15 +1,9 @@
 void main(List<String> args) {
-  // 打印所有传入的命令行参数
-  print('命令行参数列表：$args');
-  
-  // 如果需要使用整数，可以将字符串转换为整数
-  for (var arg in args) {
-    // 尝试将字符串转换为整数
-    try {
-      int number = int.parse(arg);
-      print('参数转换为数字: $number');
-    } catch (e) {
-      print('参数 $arg 不能转换为数字');
-    }
-  }
+  bool flag = true;
+  String c = flag ? 'a' : 'b';
+  print(c);
+
+  var a;
+  var b = a ?? 'b';
+  print(b);
 }
