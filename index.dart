7bac80cb8ad9b5d034e6148c25359027dc542fd7
朱科羽 <379389113@qq.com>
2024-12-04 15:02:31 +0800

    list方法

--- index.dart
+++ index.dart
@@ -1,5 +1,17 @@
 void main(List<String> args) {
-    var a = 1;
-    int b = 2;
-    print(a+b);
+  List myList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  print(myList);
+  print(myList.length);
+  print(myList.isEmpty);
+  print(myList.isNotEmpty);
+  print(myList.reversed);
+  var myNewList = myList.reversed.toList();
+  print(myNewList);
+  print(myList.first);
+  print(myList.last);
+  print(myList.indexOf('ae'));
+  print(myList.lastIndexOf('a'));
+  print(myList.sublist(2, 5));
+  myList.fillRange(0, 1, 'z');
+  print(myList);
 }
