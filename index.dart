4f44cbf255f3014f9f243577b56f738457923fc5
朱科羽 <379389113@qq.com>
2024-12-04 18:33:25 +0800

    Reapply "set"
    This reverts commit ebb599db8d0b1e64b49e6267b78ced79064a9db4.

--- index.dart
+++ index.dart
@@ -1,17 +1,5 @@
 void main(List<String> args) {
-  List myList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  print(myList);
-  print(myList.length);
-  print(myList.isEmpty);
-  print(myList.isNotEmpty);
-  print(myList.reversed);
-  var myNewList = myList.reversed.toList();
-  print(myNewList);
-  print(myList.first);
-  print(myList.last);
-  print(myList.indexOf('ae'));
-  print(myList.lastIndexOf('a'));
-  print(myList.sublist(2, 5));
-  myList.fillRange(0, 1, 'z');
-  print(myList);
+  var set = Set.from([1, 2, 3, 4, 2, 3, 4, 5]);
+  print(set);
+  
 }
