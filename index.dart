bde81d19f606c58cce94b11f28205113823aeb3f
朱科羽 <379389113@qq.com>
2024-12-05 10:36:01 +0800

    默认参数

--- index.dart
+++ index.dart
@@ -1,10 +1,11 @@
 void main(List<String> args) {
-  var userInfo = printUserInfo('홍길동', 20,'女','永林二区');
+  var userInfo = printUserInfo('홍길동', 20, '女', '永林二区');
   print(userInfo);
 }
 
-String printUserInfo(String name, int age, [String? gender, String? address]) {
-  var result = 'name: $name, age: $age';
+String printUserInfo(String name, int age,
+    [String? gender, String? address, String nationality = '中国']) {
+  var result = 'name: $name, age: $age, nationality: $nationality';
   if (gender != null) result += ', gender: $gender';
   if (address != null) result += ', address: $address';
   return result;
