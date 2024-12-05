ef7069e7f232b9fcbdf014fb823464d82e173271
朱科羽 <379389113@qq.com>
2024-12-05 19:22:28 +0800

    默认构造函数和命名构造函数

--- index.dart
+++ index.dart
@@ -1,15 +1,31 @@
 class Person {
   String name = 'tonny';
   int age = 39;
+  Person(name, age) {
+    this.name = name;
+    this.age = age;
+    print('我是默认构造函数');
+  }
+  Person.now() {
+    print('我是命名构造函数');
+  }
+  Person.setInfo(String name, int age) {
+    print('我也是命名构造函数');
+    this.name = name;
+    this.age = age;
+  }
   void getInfo() {
     print('name:$name,age:$age');
     print('name:${this.name},age:${this.age}');
-    
   }
 }
 
 void main(List<String> args) {
-  Person p = new Person();
+  // Person p = Person('tonny', 39);
+  // p.getInfo();
+  // Person p2 = Person('brook', 56);
+  // p2.getInfo();
+
+  Person p = new Person.setInfo('keyu', 47);
   p.getInfo();
 }
-                    
